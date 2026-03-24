cab0e1f19a20aabf3264afd376d0821cc91feb28
Manvendra <manvendrasingh1421@gmail.com>
2026-03-25 00:17:05 +0530

    adding fn to see versions

--- dog.go
+++ dog.go
@@ -1,5 +1,11 @@
 package dog
 
-func BarkGrownUp() string{
+import "fmt"
+
+func BarkGrownUp() string {
 	return "Bhauw Bhauw Bhauw!!"
+}
+
+func From02() {
+	fmt.Println("i m from version 0.2.0")
 }
